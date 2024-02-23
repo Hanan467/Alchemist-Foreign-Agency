cc5e6b47fc8cd6866731f2d22a75e4e0c1e9d141
Hanan <123656973+Hanan467@users.noreply.github.com>
2024-02-23 22:45:38 +0300

    crowdfund.sql

--- crowdfund.sql
+++ crowdfund.sql
@@ -36,18 +36,6 @@ CREATE TABLE `backing` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `backing`
---
-
-INSERT INTO `backing` (`Backing ID`, `Campaign_Id`, `User_Id`, `Amount_pledged`, `Timestamp`) VALUES
-(1, 2, 3, 25000, '2024-02-18 13:38:27'),
-(2, 2, 4, 30000, '2024-02-19 13:16:23'),
-(5, 5, 2, 6700, '2024-02-23 18:58:14'),
-(6, 5, 4, 6500, '2024-02-23 18:59:27'),
-(7, 6, 4, 2300, '2024-02-23 19:01:20'),
-(9, 4, 2, 2400, '2024-02-23 19:13:07'),
-(10, 4, 4, 5000, '2024-02-23 19:17:16');
-
 -- --------------------------------------------------------
 
 --
@@ -64,16 +52,6 @@ CREATE TABLE `campaign` (
   `Image` varchar(20) NOT NULL,
   `Category` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `campaign`
---
-
-INSERT INTO `campaign` (`Campaign_Id`, `User_Id`, `Title`, `Description`, `Funding_goal`, `Start_date`, `Image`, `Category`) VALUES
-(2, 2, 'school construction for rural villages in Ethiopia', 'this project is for building preparatory schools in rural Ethiopia', 100000, '2024-02-20', 'download.jpg', 'Eduacation'),
-(4, 3, 'Online Coding Bootcamp for High School Students', 'This online coding bootcamp is designed to introduce high school students to the fundamentals of computer programming. Through live coding sessions, projects, and mentorship, participants will gain practical coding skills and prepare for future careers in technology.', 300000, '2024-03-04', 'bootcamp.jpg', 'Eduacation'),
-(5, 2, 'Financial Literacy Program for Teens', 'This program teaches teens essential financial literacy skills, including budgeting, saving, investing, and understanding credit. Through interactive workshops and real-life scenarios, participants will gain the knowledge and confidence to make informed financial decisions as they transition into adulthood.', 35000, '2024-02-27', 'finance.jpg', 'Eduacation'),
-(6, 2, 'Medical Equipment Donation Drive', ' This project focuses on collecting and donating medical equipment to hospitals and clinics in low-income areas. The donated equipment, such as hospital beds, wheelchairs, and medical supplies, will help healthcare facilities improve their capacity to treat patients and save lives.', 400000, '2024-03-03', 'equipments.jpg', 'Medical');
 
 -- --------------------------------------------------------
 
@@ -99,14 +77,6 @@ CREATE TABLE `profile` (
   `User_Id` int(10) NOT NULL,
   `Profile_picture` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `profile`
---
-
-INSERT INTO `profile` (`Profile Id`, `User_Id`, `Profile_picture`) VALUES
-(1, 2, 'avatar3.png'),
-(2, 3, 'avatar2.jpg');
 
 -- --------------------------------------------------------
 
@@ -139,16 +109,7 @@ CREATE TABLE `user` (
   `Paymenmt detail` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `user`
---
 
-INSERT INTO `user` (`User_Id`, `Username`, `First_name`, `Last_name`, `Email`, `Password`, `Phone_no`, `Birthday`, `Paymenmt detail`) VALUES
-(2, 'niha47', 'Hanan', 'Ansar', 'hala@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 933695089, '2023-09-29', ''),
-(3, 'hila99', 'Hila', 'Ansar', 'hila@gmail.com', '4afd521d77158e02aed37e2274b90c9c', 917931971, '2017-02-14', ''),
-(4, 'hasu98', 'Hasinet', 'Ansar', 'hasu@gmail.com', '9e1e06ec8e02f0a0074f2fcc6b26303b', 987654321, '0000-00-00', '');
-
---
 -- Indexes for dumped tables
 --
 
